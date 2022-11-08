4a876d922eecd11eb4293c1d077f0ee07eeae296
Fabian Diet <fabian.diet@spaceapplications.com>
2022-11-08 22:18:29 +0100

    v1.2.6

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setuptools.setup(
     name="sphinxcontrib-yamcs",
-    version="1.2.5",
+    version="1.2.6",
     license="BSD",
     description="Sphinx extensions for use within the Yamcs project.",
     author="Space Applications Services",
